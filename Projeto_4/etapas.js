23d71fcaa6bd9a0b224a6c8d01cdc1dee5b5b2bd
JoseGhu <69773420+JoseGhu@users.noreply.github.com>
2021-04-19 20:17:34 -0300

    Add files via upload

---
+++ Projeto_4/etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Paulo Sergio',
+                partido: 'ABC',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Tiririca',
+                partido: 'def',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ] 
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2, 
+        candidatos: [
+            {
+                numero: '45',
+                nome: 'Bruno Covas',
+                partido: 'PSDB',
+                vice: 'Ricardo Nunes',
+                fotos: [
+                    {url:'45.jpg', legenda: 'prefeito'},
+                    {url:'45_vice.jpg', legenda: 'vice-prefeito', small: true}
+                ]
+            },
+            {
+                numero: '15',
+                nome: 'Guilherme Boulos',
+                partido: 'PSOL',
+                vice: 'Eurondina',
+                fotos: [
+                    {url:'15.jpg', legenda: 'prefeito'},
+                    {url:'15_vice.jpg', legenda: 'vice-prefeito', small: true}
+                ] 
+            },
+
+        ]
+    }
+];
